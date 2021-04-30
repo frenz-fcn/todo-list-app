2e29184eb697dc2e6a240acfe977296c6b50fb96
frenz-fcn <69198586+frenz-fcn@users.noreply.github.com>
2021-05-01 02:08:51 +0800

    Create simple todo using useContext and useReducer

--- src/context.js
+++ src/context.js
@@ -1,22 +1,11 @@
-// import { createContext } from "react";
-
-// const Context = createContext({
-//   currentTodo: null,
-//   todos: [],
-// });
-
-// export default Context;
-
 import React from "react";
 
 const NotesContext = React.createContext({
   currentTodo: null,
   todos: [
-    { title: "E", description: "EE", status: "Pending" },
-    { title: "C", description: "CC", status: "Pending" },
-    { title: "A", description: "AA", status: "Pending" },
-    { title: "B", description: "BB", status: "Pending" },
-    { title: "D", description: "DD", status: "Pending" },
+    { title: "Todo 1", description: "Sample todo 1 description...", status: "Pending" },
+    { title: "Todo 2", description: "Sample todo 1 description...", status: "Pending" },
+    { title: "Todo 3", description: "Sample todo 1 description...", status: "Done" }, 
   ],
 });
 
